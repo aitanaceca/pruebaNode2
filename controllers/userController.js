1cfe9c14e5721f942c3209e43dcf03dd2c227bc9
Aitana Cerezo <43374601+aitanaceca@users.noreply.github.com>
2020-11-26 10:59:28 +0100

    :)

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,56 +20,12 @@ function new_user (req, res) {
     console.log(apar);
     if (apar >= 1) {
         console.log('Mail exists');
+        res.redirect('/user/signup');
     } else {
         databaseConnector.insert({ email: req.body.email, password: req.body.password}, {fields: ["email", "password"]});
         res.redirect('/user/signup');
     }
 };
-
-
-/*function new_user (req, res) {
-    var apar = 0;
-    mysqlConnector.findData({ email: req.body.email });
-    apar = mysqlConnector.countData({ email: req.body.email });
-    console.log(apar);
-    if (apar >= 1) {
-        console.log('Mail exists');
-        res.json('Mail exist');
-    } else {
-        mysqlConnector.insertData({ email: req.body.email, password: req.body.password }, {fields: ["email", "password"]});
-        res.redirect('/user/signup');
-    }
-};*/
-
-
-// Mongo save data
-/*const new_user = (req, res) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                console.log('Mail exists');
-                res.redirect('/user/signup')
-            } else {
-                const user = new User({
-                    _id: new mongoose.Types.ObjectId(),
-                    email: req.body.email,
-                    password: req.body.password
-                });
-                user.save()
-                    .then(result => {
-                        console.log('User created');
-                        res.redirect('/user/signup');
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
-};*/
 
 
 // app.post con encriptado
